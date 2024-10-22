99aab2f2fe1060ed11cebbcbac17306d62f144a8
Yashasvi49 <ssuhani387@gmail.com>
2024-10-22 23:31:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+// 1)
+// const x = +prompt("number1");
+// const y = +prompt("number2");
+// const sum = x+y;
+// console.log(sum);
+
+// 2)
+// const x = +prompt("number1");
+// const y = +prompt("number2");
+// const res = x+y;
+// console.log( `The sum of ${x} and ${y} is ${res}`);
+
+// 3)
+// const x = +prompt("number1");
+// const y = +prompt("number2");
+// const res = x+y;
+// console.log( `The sum of ${x} and ${y} is ${res}`);
+
+// 4)
+// let name = prompt("username");
+// let age = +prompt("age");
+// console.log(`hello ${name}, you are ${age} years old.`)
+
+// 5)
+// let i = 0;
+// let j = 0;
+// console.log(--i * i++ * ++j * j++);
+//-1*-1*1*1
+
+// 6) a
+// let num1 = +prompt("number1");
+// let num2 = +prompt("number2");
+// let temp = num1 ;
+// num1 = num2 ;
+// num2 = temp;
+// console.log(num1);
+// console.log(num2);
+
+// b)
+// let num1 = +prompt("number1");
+// let num2 = +prompt("number2");
+//  num1 = num1 + num2 ;
+//  num2 = num1 - num2 ;
+//  num1 = num1 - num2 ;
+// console.log(num1);
+// console.log(num2);
+
+
+// 7)
+// let length = prompt("length");
+// let width = prompt("width")
+// const area = length*width;
+// console.log(area);
+// const perimeter = 2*length*width;
+// console.log(perimeter);
+
+// 12)
+// let num1 = +prompt("say number1");
+// let num2 = +prompt("say number2")
+// if (num1>num2) {
+//  console.log("num1 is greater")
+// }else if (num1<num2) {
+//  console.log("num2 is greater")
+// }else {
+//  console.log("num1 is equal to num2")
+    
+// }
+
+// 13)
+// let n = prompt("gender?");
+// if (n == "female") {
+//     console.log("good morning madam")
+// }else if(n == "male") {
+//     console.log("good morning sir")
+// }else{
+//     console.log("good morning")
+// }
+
+//14
+// let gender = prompt("enter your gender")
+// if (gender=="male"||gender=="Male") {
+//     console.log("Good Morning Sir ");
+    
+//   } else if (gender=="female"||gender=="female") {
+//     console.log("Good Morning mam ");
+//   }else{
+//     console.log("Wrong Input");
+    
+//   }
+
+
+  //15
+//   let n = +prompt("Enter a number");
+
+//     if (n % 2 == 0){
+//         console.log ("Even")
+//     } else {
+//         console.log ("Odd")
+//     }
+   
+//     //16
+//     const username = prompt("Enter  Your Name") ;
+// const age = +prompt("Enter your Age") ;
+// console.log(`the username is ${username} and the age is ${age}`);
+// or
+// console.log(username , age );
+// if (age >= 18) {
+//     console.log("Eligible to vote");
+// }else {
+//     console.log(
+//                  `SORRY!! ${username} you can not vote for ${18 - age}" more years`);
+// }
+
+//17
